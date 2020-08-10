7a2ddc7df23c4852f33689d5230f15918ccbf143
Ahmad Farhat <rappar.stand@gmail.com>
2020-08-10 13:03:19 +0600

    user list table header added

--- users.php
+++ users.php
@@ -9,26 +9,36 @@
 
 <body>
     <table border="1px">
-        <?php
-        $file = fopen("data.txt", "r");
+        <thead>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>User Type</th>
 
-        $con = mysqli_connect('localhost:3306', 'root', '', 'test');
-        $sql = "select * from users ";
-
-        $result = mysqli_query($con, $sql);
-
-
-        while ($row = mysqli_fetch_assoc($result)) {
-
-            echo '<td>' . $row['id'] . '</td>';
-            echo '<td>' . $row['password'] . '</td>';
-            echo '<td>' . $row['name'] . '</td>';
-            echo '<td>' . $row['email'] . '</td>';
-            echo '<td>' . $row['userType'] . '</td>';
+        </thead>
+        <tbody>
 
 
-            echo '</tr>';
-        }
+            <?php
+            $file = fopen("data.txt", "r");
+
+            $con = mysqli_connect('localhost:3306', 'root', '', 'test');
+            $sql = "select * from users ";
+
+            $result = mysqli_query($con, $sql);
+
+
+            while ($row = mysqli_fetch_assoc($result)) {
+
+                echo '<td>' . $row['id'] . '</td>';
+                // echo '<td>' . $row['password'] . '</td>';
+                echo '<td>' . $row['name'] . '</td>';
+                echo '<td>' . $row['email'] . '</td>';
+                echo '<td>' . $row['userType'] . '</td>';
+
+
+                echo '</tr>';
+            }
 
 
 
@@ -37,8 +47,8 @@
 
 
 
-        ?>
-
+            ?>
+        </tbody>
     </table>
 </body>
 
